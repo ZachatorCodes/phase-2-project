9b2c1cf3501a6ee19bfb6360cd58ad8bc5b1426c
Zach Schwartz <zachatorcodes@gmail.com>
2023-02-22 16:47:59 -0700

    Added navigation to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Home from "./Home";
+import Navigation from "./Navigation";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Navigation />
       <Switch>
         <Route exact path="/">
           <Home />
