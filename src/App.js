27f46260cf318a335e5f1837e7ee753aa392cd82
Zach Schwartz <zachatorcodes@gmail.com>
2023-02-22 16:33:38 -0700

    Imported Home into App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Header from "./Header";
+import Home from "./Home";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
