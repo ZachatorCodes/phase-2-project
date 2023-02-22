5ab4acf55dec882a6144e10d5222f76d164d4bc7
Zach Schwartz <zachatorcodes@gmail.com>
2023-02-22 14:48:28 -0700

    Renamed class names

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
-import bunk from "./bunkface.png";
+import bunkface from "./bunkface.png";
 
 function Header() {
   return (
     <div className="Header">
       <h1 className="HeaderText">Bunk</h1>
-      <div className="bunkSpin">
-        <img src={bunk} className="bunkImage" alt="Bunk The Cat" />
+      <div className="BunkFace">
+        <img src={bunkface} className="BunkImage" alt="Bunk The Cat" />
       </div>
       <h1 className="HeaderText">Sales</h1>
     </div>
