fec8a966ac71f093fe437e9b1c645490f00f7f4f
Zach Schwartz <zachatorcodes@gmail.com>
2023-02-22 13:13:27 -0700

    Set up basic header with text and logo

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
+import bunk from "./bunkface.png";
 
 function Header() {
   return (
-    <div>
-      <h1>Bunk Sales</h1>
+    <div className="Header">
+      <h1 className="HeaderText">Bunk</h1>
+      <div className="bunkSpin">
+        <img src={bunk} className="bunkImage" alt="Bunk The Cat" />
+      </div>
+      <h1 className="HeaderText">Sales</h1>
     </div>
   );
 }
