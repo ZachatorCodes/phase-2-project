114f53f15ff1a3837fdb01ecc66556868f996c1b
Zach <zachatorcodes@gmail.com>
2023-02-28 13:24:06 -0700

    Added basic search bar JSX

--- src/GameSearch.js
+++ src/GameSearch.js
@@ -4,6 +4,18 @@ function GameSearch() {
   return (
     <div className="GameSearch">
       <h1>Game Search</h1>
+      <div className="mainSearch">
+        <h3>Please search for a game below.</h3>
+        <div className="searchForm">
+          <form>
+            <input
+              type="text"
+              className="gameInput"
+              placeholder="Enter Name Here"
+            ></input>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
