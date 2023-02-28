ada705d31214289849492eef555950ab54619541
Zach <zachatorcodes@gmail.com>
2023-02-28 13:07:12 -0700

    Fixed routing issue

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route exact path="gamesearch">
+        <Route exact path="/gamesearch">
           <GameSearch />
         </Route>
       </Switch>
