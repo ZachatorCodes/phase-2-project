95b759aa14aeafc57b3deb1eca646d4c05eb77fb
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-01 13:20:09 -0700

    Added Games component

--- src/GameContainer.js
+++ src/GameContainer.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import GameSearch from "./GameSearch";
+import Games from "./Games";
 
 function GameContainer() {
   const [search, setSearch] = useState("");
   const [games, setGames] = useState([]);
+
   return (
     <div className="GameContainer">
       <GameSearch search={search} setSearch={setSearch} setGames={setGames} />
+      <Games games={games} />
     </div>
   );
 }
