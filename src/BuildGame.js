a1c9d52d2aadb140de3b7e6616607a953d05955c
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-01 14:56:00 -0700

    Added cheapest price ever info to component

--- src/BuildGame.js
+++ src/BuildGame.js
@@ -5,6 +5,7 @@ function BuildGame({ game }) {
     <div className="GameCard" id={game.gameID}>
       <h2>{game.external}</h2>
       <img src={game.thumb} width="200px" height="75px"></img>
+      <p>Cheapest Price EVER - ${game.cheapest}</p>
     </div>
   );
 }
