93500ace7991220b9e759f6d6c32268bf756748c
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-01 14:55:35 -0700

    Added basic instructions

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ function Home() {
     <div className="Home">
       <div className="appInformation">
         <h1>Welcome to Bunk Game Sales</h1>
+        <h3>To find a game on sale, first visit the "Game Search" tab.</h3>
       </div>
     </div>
   );
