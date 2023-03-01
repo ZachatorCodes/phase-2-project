b0c81ba52d51a4c70f861812489ac8bf3e314204
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-01 14:34:22 -0700

    Added div around games

--- src/Games.js
+++ src/Games.js
@@ -2,9 +2,13 @@ import React from "react";
 import BuildGame from "./BuildGame";
 
 function Games({ games }) {
-  return games.map((game, index) => {
-    return <BuildGame game={game} key={index} />;
-  });
+  return (
+    <div className="GameStorage">
+      {games.map((game, index) => {
+        return <BuildGame game={game} key={index} />;
+      })}
+    </div>
+  );
 }
 
 export default Games;
