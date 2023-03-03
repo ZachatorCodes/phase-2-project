6411e7bc5f0ab2d920f234dde376ff0e00458364
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-03 14:02:46 -0700

    Created checkboxes for filter and sort

--- src/StoreFilter.js
+++ src/StoreFilter.js
@@ -4,6 +4,18 @@ function StoreFilter() {
   return (
     <div className="StoreFilter">
       <h1>Store Filter</h1>
+      <form>
+        <label htmlFor="sort">
+          Sort Alphabetically
+          <input type="checkbox" id="sort"></input>
+        </label>
+        <label htmlFor="filter">
+          Filter Active
+          <input type="checkbox" id="filter"></input>
+        </label>
+      </form>
     </div>
   );
 }
+
+export default StoreFilter;
