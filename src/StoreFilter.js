70d6bf6747811fd71c3c1d3698a491d1f974bf38
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-03 14:13:51 -0700

    Set up state handling

--- src/StoreFilter.js
+++ src/StoreFilter.js
@@ -1,17 +1,36 @@
 import React from "react";
 
 function StoreFilter({ activeFilter, setActiveFilter, sort, setSort }) {
+  function handleSortChange(e) {
+    setSort(e.target.checked);
+  }
+
+  function handleFilterChange(e) {
+    setActiveFilter(e.target.checked);
+  }
+
   return (
     <div className="StoreFilter">
       <h1>Store Filter</h1>
       <form>
         <label htmlFor="sort">
           Sort Alphabetically
-          <input type="checkbox" id="sort"></input>
+          <input
+            type="checkbox"
+            id="sort"
+            onChange={handleSortChange}
+            value={sort}
+          ></input>
         </label>
+        <br />
         <label htmlFor="filter">
           Filter Active
-          <input type="checkbox" id="filter"></input>
+          <input
+            type="checkbox"
+            id="filter"
+            onChange={handleFilterChange}
+            value={activeFilter}
+          ></input>
         </label>
       </form>
     </div>
