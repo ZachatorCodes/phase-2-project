c88ffbfa11f63edb67b3ac3b5fea2f23d809d3b0
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-03 13:23:12 -0700

    Added state and effect to component. Set up basic fetch

--- src/Stores.js
+++ src/Stores.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Store() {
+  const [stores, setStores] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.cheapshark.com/api/1.0/stores")
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
+        setStores(data);
+      });
+  }, []);
+
   return (
     <div className="Stores">
       <h1>Stores</h1>
