4ef4afc634d58617f7a93e060bfa95fd8d1dbda7
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-03 15:08:32 -0700

    Added images to store cards

--- src/BuildStore.js
+++ src/BuildStore.js
@@ -4,6 +4,11 @@ function BuildStore({ store }) {
   return (
     <div className="StoreCard">
       <h1>{store.storeName}</h1>
+      <img
+        className="StoreImage"
+        src={`https://www.cheapshark.com/${store.images.logo}`}
+      ></img>
+      <p>{store.isActive === 1 ? "ACTIVE" : "NOT ACTIVE"}</p>
     </div>
   );
 }
