e3881669403c80c57d55c23461452c4abba01343
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-05 14:06:28 -0700

    Imported GameDeal component and set up state

--- src/Games.js
+++ src/Games.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import BuildGame from "./BuildGame";
+import GameDeal from "./GameDeal";
 
 function Games({ games }) {
+  const [gameDeal, setGameDeal] = useState(null);
+
   return (
-    <div id="Container">
+    <div className="Container">
       <div className="GameStorage">
         {games.map((game, index) => {
-          return <BuildGame game={game} key={index} />;
+          return <BuildGame game={game} key={index} setGameDeal={setGameDeal}/>;
         })}
+      </div>
+      <div className="DealStorage">
+        <GameDeal gameID={gameDeal}/>
       </div>
     </div>
   );
