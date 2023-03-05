d971f8134adfb2ed17f5e7f68278700b757176a5
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-05 14:05:22 -0700

    Set up fetch with useEffect

--- src/GameDeal.js
+++ src/GameDeal.js
@@ -1,6 +1,16 @@
 import React, { useEffect } from "react";
 
 function GameDeal({ gameID }) {
+  useEffect(() => {
+    if (gameID !== null) {
+      fetch(`https://www.cheapshark.com/api/1.0/games?id=${gameID}`)
+        .then((r) => r.json())
+        .then((data) => {
+          console.log(data);
+        });
+    }
+  }, [gameID]);
+
   return (
     <div>
       <h1>{gameID}</h1>
