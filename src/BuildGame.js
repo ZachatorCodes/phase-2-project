7be62bffe052f3f6d1b3d56ec1e8f58ca0354e43
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-05 14:05:50 -0700

    Set up game click handling

--- src/BuildGame.js
+++ src/BuildGame.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function BuildGame({ game }) {
+function BuildGame({ game, setGameDeal }) {
+  function handleGameClick(e) {
+    setGameDeal(e.target.parentElement.id);
+  }
+
   return (
     <div className="GameCard" id={game.gameID}>
       <h2>{game.external}</h2>
       <img src={game.thumb} width="200px" height="75px"></img>
       <p>Cheapest Price EVER - ${game.cheapest}</p>
+      <button onClick={handleGameClick}>See Deals</button>
     </div>
   );
 }
