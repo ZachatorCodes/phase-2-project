b431561cc941227833c4502b6cf2826b93a3480e
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-05 17:09:16 -0700

    Set up state variables for game information

--- src/GameDeal.js
+++ src/GameDeal.js
@@ -1,19 +1,36 @@
 import React, { useState, useEffect } from "react";
 
 function GameDeal({ gameID }) {
+  const [cheapestPrice, setCheapestPrice] = useState(null);
+  const [info, setInfo] = useState(null);
+  const [deals, setDeals] = useState(null);
+
   useEffect(() => {
     if (gameID !== null) {
       fetch(`https://www.cheapshark.com/api/1.0/games?id=${gameID}`)
         .then((r) => r.json())
         .then((data) => {
           console.log(data);
+          setCheapestPrice(data.cheapestPriceEver.price);
+          setInfo(data.info);
+          setDeals(data.deals);
         });
     }
   }, [gameID]);
 
   return (
     <div className="GameDeal">
-      <h1>{gameID}</h1>
+      <div className="GameDealInfo">
+        {info !== null ? (
+          <div>
+            <h1>Title: {info.title}</h1>
+            <img src={info.thumb} />
+          </div>
+        ) : null}
+        {cheapestPrice !== null ? (
+          <h1>Cheapest Price Ever: {cheapestPrice}</h1>
+        ) : null}
+      </div>
     </div>
   );
 }
