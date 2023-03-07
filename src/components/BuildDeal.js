274b5ceba8294539c1be9011ba40d3753148d736
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-07 13:02:27 -0700

    Builds game card if deal store is active

--- src/components/BuildDeal.js
+++ src/components/BuildDeal.js
@@ -1,16 +1,18 @@
 import React from "react";
 
 function BuildDeal({ deal, stores }) {
-  const dealStore = stores.find(store => {
+  const dealStore = stores.find((store) => {
     return store.storeID === deal.storeID;
   });
-  
-  return (
-    <div className="Deal">
-      <h3>Deal</h3>
-    </div>
-  );
+  console.log(dealStore);
+  if (dealStore.isActive === 1) { 
+    return (
+      <div className="Deal" id={deal.dealID}>
+        <h3>{dealStore.storeName}</h3>
+        <img src={`https://www.cheapshark.com/${dealStore.images.logo}`} className="StoreImage"></img>
+      </div>
+    );
+  }
 }
 
 export default BuildDeal;
- 
