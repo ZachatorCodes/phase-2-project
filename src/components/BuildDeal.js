7b5318254239e949b6436ecf10aeb36cc048aefc
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-06 17:37:09 -0700

    Searches for specific store for deal

--- src/components/BuildDeal.js
+++ src/components/BuildDeal.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 function BuildDeal({ deal, stores }) {
+  const dealStore = stores.find(store => {
+    return store.storeID === deal.storeID;
+  });
+  
   return (
     <div className="Deal">
       <h3>Deal</h3>
@@ -9,3 +13,4 @@ function BuildDeal({ deal, stores }) {
 }
 
 export default BuildDeal;
+ 
