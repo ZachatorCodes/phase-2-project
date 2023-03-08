2e147a65345d60c7d2bf05604a1b47007980063f
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-08 16:16:48 -0700

    Added useEffect for fetch

--- src/components/StoreDeals.js
+++ src/components/StoreDeals.js
@@ -2,7 +2,16 @@ import React, { useEffect, useState } from "react";
 
 function StoreDeals({ storeID }) {
   const [listOfDeals, setListOfDeals] = useState(null);
-  
+  useEffect(() => {
+    if (storeID !== null) {
+      fetch(`https://www.cheapshark.com/api/1.0/deals?storeID=${storeID}`)
+        .then(r => r.json())
+        .then(data => {
+          console.log(data);
+          setListOfDeals(data);
+        })
+    }
+  }, [storeID]);
   return (
     <div className="StoreDeals">
       <h1>StoreDeals</h1>
