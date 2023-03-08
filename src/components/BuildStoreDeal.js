7f10c34a5447b2b4325c6e09e894e6205a0e6df3
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-08 16:28:20 -0700

    Completed building store deal cards

--- src/components/BuildStoreDeal.js
+++ src/components/BuildStoreDeal.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-function BuildStoreDeal() {
+function BuildStoreDeal({ deal }) {
   return (
     <div className="BuildStoreDeal">
-      <h1>BuildStoreDeal</h1>
+      <div className="Deal">
+        <h1>{deal.title}</h1>
+        <img src={deal.thumb}></img>
+        <p>Regular Price: ${deal.normalPrice}</p>
+          <p>Sale Price: ${deal.salePrice}</p>
+          <p>
+            Discount: $
+            {(parseFloat(deal.normalPrice) - parseFloat(deal.salePrice)).toFixed(2)}
+          </p>
+      </div>
     </div>
   );
 }
