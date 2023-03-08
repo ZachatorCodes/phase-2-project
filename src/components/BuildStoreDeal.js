bea4b32094506fd9211d280515e35223620fc599
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-08 16:18:10 -0700

    Created component to build store specific deals

---
+++ src/components/BuildStoreDeal.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BuildStoreDeal() {
+  return (
+    <div className="BuildStoreDeal">
+      <h1>BuildStoreDeal</h1>
+    </div>
+  );
+}
+
+export default BuildStoreDeal;
