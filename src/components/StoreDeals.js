a718c5c87110a469b3c3d7a4f02499d4e73febcb
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-08 16:16:33 -0700

    Added state

--- src/components/StoreDeals.js
+++ src/components/StoreDeals.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function StoreDeals() {
+function StoreDeals({ storeID }) {
+  const [listOfDeals, setListOfDeals] = useState(null);
+  
   return (
     <div className="StoreDeals">
       <h1>StoreDeals</h1>
+      <h1>{storeID}</h1>
+      <div className="Deals">
+        
+      </div>
     </div>
-  )
+  );
 }
 
 export default StoreDeals;
