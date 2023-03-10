5640dba4c30327bee9e59b6dd1d5e99d84ad7c0d
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-09 18:56:50 -0700

    Imported BuildWishlistApp and builds a card for each game on the wishlist

--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -1,17 +1,23 @@
 import React, { useEffect } from "react";
+import BuildWishlistApp from "./BuildWishlistApp";
 
-function Wishlist({ wishlist }) {
+function Wishlist({ wishlist, setWishlist }) {
   useEffect(() => {
     fetch("http://localhost:4000/games")
       .then((r) => r.json())
       .then((data) => {
-        console.log(data);
+        setWishlist(data);
       });
   }, [wishlist]);
 
   return (
     <div className="Wishlist">
       <h1>Wishlist</h1>
+      <div className="Deals">
+        {wishlist.map((game, index) => {
+          return <BuildWishlistApp game={game} key={index}/>;
+        })}
+      </div>
     </div>
   );
 }
