502c0dce205bb2990fe19a619d4eb955500b5f93
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-09 18:34:15 -0700

    Added useEffect to fetch wishlist games

--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function Wishlist() {
+function Wishlist({ wishlist }) {
+  useEffect(() => {
+    fetch("http://localhost:4000/games")
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, [wishlist]);
+
   return (
     <div className="Wishlist">
       <h1>Wishlist</h1>
     </div>
-  )
+  );
 }
 
 export default Wishlist;
