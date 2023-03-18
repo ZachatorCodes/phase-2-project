87b0aac455a5c68f9f595306c60581411ef20d81
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-18 15:11:27 -0600

    Removed unnessary state variables

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,12 @@ import Header from "./Header";
 import Home from "./Home";
 import Navigation from "./Navigation";
 import GameContainer from "./GameContainer";
-import Store from "./Stores";
+import Stores from "./Stores";
 import Favorites from "./Favorites";
-import Form from "./Form";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
   const [stores, setStores] = useState([]);
-  const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
     fetch("https://www.cheapshark.com/api/1.0/stores")
@@ -29,10 +27,10 @@ function App() {
           <GameContainer stores={stores} />
         </Route>
         <Route exact path="/stores">
-          <Store stores={stores} setStores={setStores} />
+          <Stores stores={stores} />
         </Route>
         <Route exact path="/favorites">
-          <Favorites favorites={favorites} />
+          <Favorites />
         </Route>
         <Route exact path="/">
           <Home />
