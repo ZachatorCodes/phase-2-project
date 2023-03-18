cd11f2e90991192c4f1621adbf2e2df9d60f8851
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-18 15:22:12 -0600

    Changed component name and references

--- src/components/GameSearch.js
+++ src/components/GameSearch.js
@@ -1,36 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import SearchBar from "./SearchBar";
+import Games from "./Games";
 
-function GameSearch({ search, setSearch, setGames }) {
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log("SUBMITTED:", search);
-    setSearch("");
-    fetch(`https://www.cheapshark.com/api/1.0/games?title=${search}`)
-      .then((r) => r.json())
-      .then((data) => {
-        console.log(data);
-        setGames(data);
-      });
-  }
+function GameSearch({stores}) {
+  const [search, setSearch] = useState("");
+  const [games, setGames] = useState([]);
 
   return (
     <div className="GameSearch">
-      <h1 className="searchTitle">Game Search</h1>
-      <div className="mainSearch">
-        <h3>Please search for a game below.</h3>
-        <div className="searchForm">
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              className="gameInput"
-              placeholder="Enter Name Here"
-              onChange={(e) => setSearch(e.target.value)}
-              value={search}
-            ></input>
-            <input type="submit" value="Submit"></input>
-          </form>
-        </div>
-      </div>
+      <SearchBar search={search} setSearch={setSearch} setGames={setGames} />
+      <Games games={games} stores={stores}/>
     </div>
   );
 }
