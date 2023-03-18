5a1f96dccdb862e94a216bcbde1ead270b12f54a
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-18 15:14:17 -0600

    Removed unnessary active or not checking

--- src/components/BuildStoreDeal.js
+++ src/components/BuildStoreDeal.js
@@ -1,10 +1,6 @@
 import React from "react";
 
-function BuildStoreDeal({ deal, stores }) {
-  const dealStore = stores.find((store) => {
-    return store.storeID === deal.storeID;
-  });
-
+function BuildStoreDeal({ deal }) {
   function visitDeal() {
     window.open(
       `https://www.cheapshark.com/redirect?dealID=${deal.dealID}`,
@@ -12,26 +8,24 @@ function BuildStoreDeal({ deal, stores }) {
     );
   }
 
-  if (dealStore.isActive === 1) {
-    if (parseFloat(deal.savings) !== 0) {
-      return (
-        <div className="BuildStoreDeal">
-          <div className="Deal">
-            <h1>{deal.title}</h1>
-            <img src={deal.thumb}></img>
-            <p>Regular Price: ${deal.normalPrice}</p>
-            <p>Sale Price: ${deal.salePrice}</p>
-            <p>
-              Discount: $
-              {(
-                parseFloat(deal.normalPrice) - parseFloat(deal.salePrice)
-              ).toFixed(2)}
-            </p>
-            <button onClick={visitDeal}>Visit Deal</button>
-          </div>
+  if (parseFloat(deal.savings) !== 0) {
+    return (
+      <div className="BuildStoreDeal">
+        <div className="Deal">
+          <h1>{deal.title}</h1>
+          <img src={deal.thumb}></img>
+          <p>Regular Price: ${deal.normalPrice}</p>
+          <p>Sale Price: ${deal.salePrice}</p>
+          <p>
+            Discount: $
+            {(
+              parseFloat(deal.normalPrice) - parseFloat(deal.salePrice)
+            ).toFixed(2)}
+          </p>
+          <button onClick={visitDeal}>Visit Deal</button>
         </div>
-      );
-    }
+      </div>
+    );
   }
 }
 
