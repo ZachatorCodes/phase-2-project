67588ca7236777624d89b505bad9d4ca2b2a9236
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-18 15:13:06 -0600

    Added checking to make sure savings does not equal 0

--- src/components/BuildStoreDeal.js
+++ src/components/BuildStoreDeal.js
@@ -1,26 +1,38 @@
 import React from "react";
 
-function BuildStoreDeal({ deal }) {
+function BuildStoreDeal({ deal, stores }) {
+  const dealStore = stores.find((store) => {
+    return store.storeID === deal.storeID;
+  });
+
   function visitDeal() {
-    window.open(`https://www.cheapshark.com/redirect?dealID=${deal.dealID}`, "_blank");
+    window.open(
+      `https://www.cheapshark.com/redirect?dealID=${deal.dealID}`,
+      "_blank"
+    );
   }
-  return (
-    <div className="BuildStoreDeal">
-      <div className="Deal">
-        <h1>{deal.title}</h1>
-        <img src={deal.thumb}></img>
-        <p>Regular Price: ${deal.normalPrice}</p>
-        <p>Sale Price: ${deal.salePrice}</p>
-        <p>
-          Discount: $
-          {(parseFloat(deal.normalPrice) - parseFloat(deal.salePrice)).toFixed(
-            2
-          )}
-        </p>
-        <button onClick={visitDeal}>Visit Deal</button>
-      </div>
-    </div>
-  );
+
+  if (dealStore.isActive === 1) {
+    if (parseFloat(deal.savings) !== 0) {
+      return (
+        <div className="BuildStoreDeal">
+          <div className="Deal">
+            <h1>{deal.title}</h1>
+            <img src={deal.thumb}></img>
+            <p>Regular Price: ${deal.normalPrice}</p>
+            <p>Sale Price: ${deal.salePrice}</p>
+            <p>
+              Discount: $
+              {(
+                parseFloat(deal.normalPrice) - parseFloat(deal.salePrice)
+              ).toFixed(2)}
+            </p>
+            <button onClick={visitDeal}>Visit Deal</button>
+          </div>
+        </div>
+      );
+    }
+  }
 }
 
 export default BuildStoreDeal;
