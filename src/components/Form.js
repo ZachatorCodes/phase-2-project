008c00d91e2b46c535539073a865f2d15b2b7120
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-18 15:15:22 -0600

    Added CRUD handling to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Form() {
+function Form({ onAddGame }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const gameData = {
+      title: name,
+      image: image,
+    };
+    fetch("http://localhost:4000/games", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(gameData),
+    })
+      .then((r) => r.json())
+      .then((data) => onAddGame(data));
+  }
+
   return (
-    <div className="Form">
-      <h1>Form</h1>
+    <div className="Form" onSubmit={handleSubmit}>
+      <form>
+        <input
+          type="text"
+          name="name"
+          placeholder="Game Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <input
+          type="text"
+          image="image"
+          placeholder="Game Image URL"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        ></input>
+        <button type="submit">Add Game</button>
+      </form>
     </div>
   );
 }
