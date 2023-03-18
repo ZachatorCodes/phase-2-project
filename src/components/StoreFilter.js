25a419f5df5f66ba11facaf201fd44feb74f45cc
Zach Schwartz <zachatorcodes@gmail.com>
2023-03-18 15:16:05 -0600

    Removed active filter

--- src/components/StoreFilter.js
+++ src/components/StoreFilter.js
@@ -1,12 +1,8 @@
 import React from "react";
 
-function StoreFilter({ activeFilter, setActiveFilter, sort, setSort }) {
+function StoreFilter({ sort, setSort }) {
   function handleSortChange(e) {
     setSort(e.target.checked);
-  }
-
-  function handleFilterChange(e) {
-    setActiveFilter(e.target.checked);
   }
 
   return (
@@ -19,16 +15,6 @@ function StoreFilter({ activeFilter, setActiveFilter, sort, setSort }) {
             id="sort"
             onChange={handleSortChange}
             value={sort}
-          ></input>
-        </label>
-        <br />
-        <label htmlFor="filter">
-          Filter Active
-          <input
-            type="checkbox"
-            id="filter"
-            onChange={handleFilterChange}
-            value={activeFilter}
           ></input>
         </label>
       </form>
