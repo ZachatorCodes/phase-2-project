a7253e4cb4e498043e9122679b7c37a570f24ae1
Zach <zachatorcodes@gmail.com>
2023-03-17 22:45:15 -0600

    Added state variables

--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -1,12 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import BuildWishlistApp from "./BuildWishlistApp";
 
-function Wishlist({ wishlist, setWishlist }) {
+function Wishlist({ wishlist }) {
+  const [wishlistToDisplay, setWishlistToDisplay] = useState([]);
   useEffect(() => {
     fetch("http://localhost:4000/games")
       .then((r) => r.json())
       .then((data) => {
-        setWishlist(data);
+        setWishlistToDisplay(data);
       });
   }, [wishlist]);
 
@@ -14,7 +15,7 @@ function Wishlist({ wishlist, setWishlist }) {
     <div className="Wishlist">
       <h1>Wishlist</h1>
       <div className="Deals">
-        {wishlist.map((game, index) => {
+        {wishlistToDisplay.map((game, index) => {
           return <BuildWishlistApp game={game} key={index}/>;
         })}
       </div>
